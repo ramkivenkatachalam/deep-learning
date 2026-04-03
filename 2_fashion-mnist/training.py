c9aa522e62b8a5e4090d53c262e01e53bce3ec98
ramkivenkatachalam <ramki@utexas.edu>
2026-04-02 19:34:19 -0700

    Add BatchNorm after conv layers and Dropout(0.3) before output

--- 2_fashion-mnist/training.py
+++ 2_fashion-mnist/training.py
@@ -44,11 +44,14 @@ val_split = 0.2
 
 input = keras.Input(shape=(28, 28, 1))
 h = keras.layers.Conv2D(32, (3, 3), activation="relu", padding="same")(input)
+h = keras.layers.BatchNormalization()(h)
 h = keras.layers.MaxPooling2D((2, 2))(h)
 h = keras.layers.Conv2D(64, (3, 3), activation="relu", padding="same")(h)
+h = keras.layers.BatchNormalization()(h)
 h = keras.layers.MaxPooling2D((2, 2))(h)
 h = keras.layers.Flatten()(h)
 h = keras.layers.Dense(128, activation="relu")(h)
+h = keras.layers.Dropout(0.3)(h)
 output = keras.layers.Dense(10, activation="softmax")(h)
 model = keras.Model(input, output)
 
